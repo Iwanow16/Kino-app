14ab72c2c928f0c72f0168117e8d6275ae037c27
iwanow23 <iwanov-23@bk.ru>
2023-06-26 13:59:20 +0300

    Добавление корутинов

--- app/src/main/java/kinomaxi/feature/mainPage/view/MainPageViewModel.kt
+++ app/src/main/java/kinomaxi/feature/mainPage/view/MainPageViewModel.kt
@@ -2,119 +2,62 @@ package kinomaxi.feature.mainPage.view
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import kinomaxi.feature.movieList.data.MoviesListApiService
 import kinomaxi.feature.movieList.domain.GetMoviesListUseCase
-import kinomaxi.feature.movieList.model.MoviesList
 import kinomaxi.feature.movieList.model.MoviesListType
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.onSubscription
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class MainPageViewModel(
     private val getMoviesList: GetMoviesListUseCase,
 ) : ViewModel() {
 
-    private var topRatedMovies: MoviesList? = null
-    private var popularMovies: MoviesList? = null
-    private var upcomingMovies: MoviesList? = null
-
-    private var viewState: MainPageState = MainPageState.Loading
-        set(value) {
-            field = value
-            viewStateChangeListener(value)
-        }
-
-    private var viewStateChangeListener: (MainPageState) -> Unit = {}
-
-    fun setViewStateChangeListener(listener: (MainPageState) -> Unit) {
-        viewStateChangeListener = listener
-        viewStateChangeListener(viewState)
-    }
-
-    init {
-        loadData()
-    }
+    private var _viewState = MutableStateFlow<MainPageState>(MainPageState.Loading)
+    val viewState: Flow<MainPageState> = _viewState.asStateFlow()
+        .onSubscription { loadData() }
+        .onStart {  }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.Eagerly,
+            MainPageState.Loading
+        )
 
     fun refreshData() {
-        viewState = MainPageState.Loading
+        _viewState.value = MainPageState.Loading
         loadData()
     }
 
     private fun loadData() {
-        loadTopRatedMovies()
-        loadPopularMovies()
-        loadUpcomingMovies()
-    }
+        viewModelScope.launch {
+            try {
 
-    private fun loadTopRatedMovies() {
-        getMoviesList(
-            MoviesListType.TOP_RATED_MOVIES,
-            onSuccess = { list ->
-                topRatedMovies = list.takeUnless { viewState == MainPageState.Error }
+                val (topRatedMovies, popularMovies, upcomingMovies) = awaitAll(
+                    async { getMoviesList(MoviesListType.TOP_RATED_MOVIES) },
+                    async { getMoviesList(MoviesListType.POPULAR_MOVIES) },
+                    async { getMoviesList(MoviesListType.UPCOMING_MOVIES) },
+                )
 
-                val popularMovies = popularMovies
-                val upcomingMovies = upcomingMovies
-                if (popularMovies != null && upcomingMovies != null) {
-                    val viewData = MainPageData(
-                        topRatedMoviesList = list,
-                        topPopularMoviesList = popularMovies,
-                        topUpcomingMoviesList = upcomingMovies,
-                    )
-                    viewState = MainPageState.Success(viewData)
-                }
-            },
-            onFailure = {
-                topRatedMovies = null
-                viewState = MainPageState.Error
+                val viewData = MainPageData(
+                    topRatedMoviesList = topRatedMovies,
+                    topPopularMoviesList = popularMovies,
+                    topUpcomingMoviesList = upcomingMovies,
+                )
+                _viewState.value = MainPageState.Success(viewData)
+            } catch (e: Exception) {
+                _viewState.value = MainPageState.Error
             }
-        )
-    }
-
-    private fun loadPopularMovies() {
-        getMoviesList(
-            MoviesListType.POPULAR_MOVIES,
-            onSuccess = { list ->
-                popularMovies = list.takeUnless { viewState == MainPageState.Error }
-
-                val topRatedMovies = topRatedMovies
-                val upcomingMovies = upcomingMovies
-                if (topRatedMovies != null && upcomingMovies != null) {
-                    val viewData = MainPageData(
-                        topRatedMoviesList = topRatedMovies,
-                        topPopularMoviesList = list,
-                        topUpcomingMoviesList = upcomingMovies,
-                    )
-                    viewState = MainPageState.Success(viewData)
-                }
-            },
-            onFailure = {
-                topRatedMovies = null
-                viewState = MainPageState.Error
-            }
-        )
-    }
-
-    private fun loadUpcomingMovies() {
-        getMoviesList(
-            MoviesListType.UPCOMING_MOVIES,
-            onSuccess = { list ->
-                upcomingMovies = list.takeUnless { viewState == MainPageState.Error }
-
-                val topRatedMovies = topRatedMovies
-                val popularMovies = popularMovies
-                if (topRatedMovies != null && popularMovies != null) {
-                    val viewData = MainPageData(
-                        topRatedMoviesList = topRatedMovies,
-                        topPopularMoviesList = popularMovies,
-                        topUpcomingMoviesList = list,
-                    )
-                    viewState = MainPageState.Success(viewData)
-                }
-            },
-            onFailure = {
-                topRatedMovies = null
-                viewState = MainPageState.Error
-            }
-        )
+        }
     }
 
     companion object {
--- app/src/main/java/kinomaxi/feature/movieList/domain/GetMoviesListUseCase.kt
+++ app/src/main/java/kinomaxi/feature/movieList/domain/GetMoviesListUseCase.kt
@@ -7,9 +7,6 @@ import kinomaxi.feature.movieList.data.RestMoviesListResponse
 import kinomaxi.feature.movieList.model.Movie
 import kinomaxi.feature.movieList.model.MoviesList
 import kinomaxi.feature.movieList.model.MoviesListType
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
  * Бизнес-сценарий получения списка фильмов
@@ -21,40 +18,14 @@ class GetMoviesListUseCase(
     /**
      * Получить список фильмов с типом [listType]
      */
-    operator fun invoke(
+    suspend operator fun invoke(
         listType: MoviesListType,
-        onSuccess: (list: MoviesList) -> Unit,
-        onFailure: () -> Unit
-    ) {
-        val moviesListApiCall = when (listType) {
+    ) : MoviesList {
+        return when (listType) {
             MoviesListType.TOP_RATED_MOVIES -> apiService.getTopRatedMovies()
             MoviesListType.POPULAR_MOVIES -> apiService.getPopularMovies()
             MoviesListType.UPCOMING_MOVIES -> apiService.getUpcomingMovies()
-        }
-
-        moviesListApiCall.enqueue(object : Callback<RestMoviesListResponse> {
-            override fun onResponse(
-                call: Call<RestMoviesListResponse>,
-                response: Response<RestMoviesListResponse>
-            ) {
-                if (!response.isSuccessful) {
-                    onFailure()
-                    return
-                }
-
-                val list = response.body()?.toEntity(listType)
-                if (list == null) {
-                    onFailure()
-                    return
-                }
-
-                onSuccess(list)
-            }
-
-            override fun onFailure(call: Call<RestMoviesListResponse>, t: Throwable) {
-                onFailure()
-            }
-        })
+        }::toEntity.invoke(listType)
     }
 }
 
