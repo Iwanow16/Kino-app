183bdcb7657a10330016d43fc226c454d68eb5e3
iwanow23 <iwanov-23@bk.ru>
2023-07-03 14:36:37 +0300

    Изменение API

--- app/src/main/java/kinomaxi/feature/movieDetails/data/MovieDetailsApiService.kt
+++ app/src/main/java/kinomaxi/feature/movieDetails/data/MovieDetailsApiService.kt
@@ -1,6 +1,8 @@
 package kinomaxi.feature.movieDetails.data
 
+import kinomaxi.AppConfig
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 
 /**
@@ -11,12 +13,14 @@ interface MovieDetailsApiService {
     /**
      * Получить подробную информацию о фильме с идентификатором [movieId]
      */
+    @Headers("Authorization: Bearer ${AppConfig.BEARER_TOKEN}")
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(@Path("movie_id") movieId: Long): RestMovieDetails
 
     /**
      * Получить список изображений для фильма с идентификатором [movieId]
      */
+    @Headers("Authorization: Bearer ${AppConfig.BEARER_TOKEN}")
     @GET("movie/{movie_id}/images?include_image_language=en,null")
     suspend fun getMovieImages(@Path("movie_id") movieId: Long): RestMovieImagesResponse
 }
--- app/src/main/java/kinomaxi/feature/movieList/data/MoviesListApiService.kt
+++ app/src/main/java/kinomaxi/feature/movieList/data/MoviesListApiService.kt
@@ -1,6 +1,8 @@
 package kinomaxi.feature.movieList.data
 
+import kinomaxi.AppConfig
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 /**
  * Интерфейс взаимодействия с REST API для функционала списков фильмов
@@ -10,19 +12,21 @@ interface MoviesListApiService {
     /**
      * Получить список текущих популярных фильмов
      */
+    @Headers("Authorization: Bearer ${AppConfig.BEARER_TOKEN}")
     @GET("movie/popular")
     suspend fun getPopularMovies(): RestMoviesListResponse
 
     /**
      * Получить список фильмов с самым высоким рейтингом
      */
+    @Headers("Authorization: Bearer ${AppConfig.BEARER_TOKEN}")
     @GET("movie/top_rated")
     suspend fun getTopRatedMovies(): RestMoviesListResponse
 
     /**
      * Получить список ещё не вышедших фильмов
      */
+    @Headers("Authorization: Bearer ${AppConfig.BEARER_TOKEN}")
     @GET("movie/upcoming")
     suspend fun getUpcomingMovies(): RestMoviesListResponse
-
 }
